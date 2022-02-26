5ea34bc7aff7a1d37f53959e2122e7040d8a4b82
neoan <stefan.roehrl@gmail.com>
2022-02-26 16:09:53 -0500

    Binary detection update to allow for better i18n

--- DbOps.php
+++ DbOps.php
@@ -288,6 +288,7 @@ class DbOps
      */
     public static function isBinary($str)
     {
-        return preg_match('~[^\x20-\x7E\t\r\n]~', $str) > 0;
+//         return preg_match('~[^\x20-\x7E\t\r\n]~', $str) > 0;
+        return !preg_match('//u', $str);
     }
 }
